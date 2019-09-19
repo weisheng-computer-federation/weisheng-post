613925475cd0c249f42e7375c2df1d49fdf2abc1
luawig <2453576227@qq.com>
2019-09-19 12:46:31 +0800

    Site updated: 2019-09-19 12:46:30

--- js/main.js
+++ js/main.js
@@ -1,106 +1,37 @@
-/**
- * Sets up Justified Gallery.
- */
-if (!!$.prototype.justifiedGallery) {
-  var options = {
-    rowHeight: 140,
-    margins: 4,
-    lastRow: 'justify'
-  };
-  $('.article-gallery').justifiedGallery(options);
-}
+// To make images retina, add a class "2x" to the img element
+// and add a <image-name>@2x.png image. Assumes jquery is loaded.
 
 
-$(document).ready(function() {
-
-  /**
-   * Shows the responsive navigation menu on mobile.
-   */
-  $("#header > #nav > ul > .icon").click(function() {
-    $("#header > #nav > ul").toggleClass("responsive");
-  });
-
-
-  /**
-   * Controls the different versions of  the menu in blog post articles 
-   * for Desktop, tablet and mobile.
-   */
-  if ($(".post").length) {
-    /**
-     * Display the menu if the menu icon is clicked.
-     */
-    var menu = $("#menu");
-    var menu_icon = $("#menu-icon, #menu-icon-tablet");
-    menu_icon.click(function() {
-      if (menu.css('visibility') === 'hidden') {
-        menu.css("visibility", "visible");
-        menu_icon.addClass('active');
-      } else {
-        menu.css("visibility", "hidden");
-        menu_icon.removeClass('active');
-      }
-      return false;
-    });
-
-    /**
-     * Add a scroll listener to the menu to hide/show the navigation links.
-     */
-    if (menu.length) {
-      $(window).on('scroll', function() {
-        var topDistance = $("#menu > #nav").offset().top;
-
-        // hide only the navigation links on desktop
-        if (menu.css('visibility') !== 'hidden' && topDistance < 50) {
-          $("#menu > #nav").show();
-        } else if (menu.css('visibility') !== 'hidden' && topDistance > 100) {
-          $("#menu > #nav").hide();
-        }
-
-        // on tablet, hide the navigation icon as well and show a "scroll to top
-        // icon" instead
-        if ( ! $( "#menu-icon" ).is(":visible") && topDistance < 50 ) {
-          $("#menu-icon-tablet").show();
-          $("#top-icon-tablet").hide();
-        } else if (! $( "#menu-icon" ).is(":visible") && topDistance > 100) {
-          $("#menu-icon-tablet").hide();
-          $("#top-icon-tablet").show();
-        }
-      });
-    }
-
-    /**
-     * Show mobile navigation menu after scrolling upwards,
-     * hide it again after scrolling downwards.
-     */
-    if ($( "#footer-post").length) {
-      var lastScrollTop = 0;
-      $(window).on('scroll', function() {
-        var topDistance = $(window).scrollTop();
-
-        if (topDistance > lastScrollTop){
-          // downscroll -> show menu
-          $("#footer-post").hide();
-        } else {
-          // upscroll -> hide menu
-          $("#footer-post").show();
-        }
-        lastScrollTop = topDistance;
-
-        // close all submenu's on scroll
-        $("#nav-footer").hide();
-        $("#toc-footer").hide();
-        $("#share-footer").hide();
-
-        // show a "navigation" icon when close to the top of the page, 
-        // otherwise show a "scroll to the top" icon
-        if (topDistance < 50) {
-          $("#actions-footer > ul > #top").hide();
-          $("#actions-footer > ul > #menu").show();
-        } else if (topDistance > 100) {
-          $("#actions-footer > ul > #menu").hide();
-          $("#actions-footer > ul > #top").show();
-        }
-      });
-    }
-  }
-});
+function isRetina() {
+	var mediaQuery = "(-webkit-min-device-pixel-ratio: 1.5),\
+					  (min--moz-device-pixel-ratio: 1.5),\
+					  (-o-min-device-pixel-ratio: 3/2),\
+					  (min-resolution: 1.5dppx)";
+ 
+	if (window.devicePixelRatio > 1)
+		return true;
+ 
+	if (window.matchMedia && window.matchMedia(mediaQuery).matches)
+		return true;
+ 
+	return false;
+};
+ 
+ 
+function retina() {
+	
+	if (!isRetina())
+		return;
+	
+	$("img.2x").map(function(i, image) {
+		
+		var path = $(image).attr("src");
+		
+		path = path.replace(".png", "@2x.png");
+		path = path.replace(".jpg", "@2x.jpg");
+		
+		$(image).attr("src", path);
+	});
+};
+ 
+$(document).ready(retina);
